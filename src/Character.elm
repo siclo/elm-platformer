44f94693d5cd49f7b27ea454c8a323c8ffcb44be
siclo <simon.cloutier@gmail.com>
2016-07-13 22:00:59 -0400

    Jump (#1)
    Implemented jump, with gravity. Can only jump when on the floor

--- src/Character.elm
+++ src/Character.elm
@@ -4,6 +4,8 @@ import Color exposing (..)
 import Time exposing (Time, inSeconds)
 import InputManager
 
+gravity = 400
+
 type alias Model =
     { posX : Float
     , posY : Float
@@ -11,6 +13,8 @@ type alias Model =
     , topSpeedX : Float
     , accelerationX : Float
     , stoppingDecelerationX : Float
+    , speedY : Float
+    , jumpSpeed : Float
     }
 
 model : Model
@@ -21,6 +25,8 @@ model =
     , topSpeedX = 150
     , accelerationX = 300
     , stoppingDecelerationX = 400
+    , speedY = 0
+    , jumpSpeed = 300
     }
 
 characterModel = rect 10 30 |> filled orange
@@ -38,11 +44,15 @@ update msg model inputManager =
     case msg of
         Tick deltaTime ->
             updateModel deltaTime inputManager model ! []
-            
+
 updateModel : Time -> InputManager.Model -> Model -> Model
 updateModel deltaTime inputManager model =
     updateSpeedX deltaTime inputManager model |>
-    updatePosX deltaTime
+    updatePosX deltaTime |>
+    jumpIfValid inputManager |>
+    applyGravity deltaTime |>
+    updatePosY deltaTime |>
+    clampOnFloor
 
 updateSpeedX : Time -> InputManager.Model -> Model -> Model
 updateSpeedX deltaTime inputManager model =
@@ -63,3 +73,29 @@ updatePosX : Time -> Model -> Model
 updatePosX deltaTime model =
     { model | posX = model.posX + model.speedX * (inSeconds deltaTime) }
 
+jumpIfValid : InputManager.Model -> Model -> Model
+jumpIfValid inputManager model =
+  if canJump inputManager model then
+    { model | speedY = model.jumpSpeed }
+  else
+    model
+
+canJump : InputManager.Model -> Model -> Bool
+canJump inputManager model =
+  inputManager.up && isOnFloor model
+
+isOnFloor : Model -> Bool
+isOnFloor model =
+  model.posY == 0
+
+applyGravity : Time -> Model -> Model
+applyGravity deltaTime model =
+  { model | speedY = model.speedY - gravity * inSeconds deltaTime }
+
+updatePosY : Time -> Model -> Model
+updatePosY deltaTime model =
+  { model | posY = model.posY + model.speedY * inSeconds deltaTime }
+
+clampOnFloor : Model -> Model
+clampOnFloor model =
+  { model | posY = max model.posY 0 }
